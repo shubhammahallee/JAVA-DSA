c8d20fb67c7087db5ae94e1b83e9c446f3e86adc
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-07 10:06:28 +0530

    Update ProductOfArray.java

--- Arrays/ProductOfArray.java
+++ Arrays/ProductOfArray.java
@@ -1,1 +1,8 @@
+ public static void productOfArray(int[] arr) {
+        long product = 1; // Changed to long to prevent overflow
+        for (int i = 0; i < arr.length; i++) {
+            product *= arr[i];
+        }
+        System.out.println("Product of Array: " + product);
+    }
 
