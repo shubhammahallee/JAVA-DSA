3ed8c4daff8399dd75ba0fc0183bed94ba6fdae7
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-07 10:05:15 +0530

    Add method to calculate sum of array elements

--- Arrays/SumOfArray.java
+++ Arrays/SumOfArray.java
@@ -1,1 +1,7 @@
-
+ public static void SumOfArray(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Sum of Array: " + sum);
+    }
