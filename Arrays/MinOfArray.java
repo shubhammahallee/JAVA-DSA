157ebac8d1a26218153eed1700741a78b172ecdb
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-08 08:45:41 +0530

    Update MinOfArray.java

--- Arrays/MinOfArray.java
+++ Arrays/MinOfArray.java
@@ -1,1 +1,9 @@
-
+ public static void minOfArray(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) { 
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum of Array: " + min);
+    }
