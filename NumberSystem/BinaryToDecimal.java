8ecb536963ce93b7df4fd407fc16b99f0624c6e7
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-09 23:04:37 +0530

    Update BinaryToDecimal.java

--- NumberSystem/BinaryToDecimal.java
+++ NumberSystem/BinaryToDecimal.java
@@ -6,7 +6,7 @@ public class BinaryToDecimal {
         int  binary_num =sc.nextInt();
         int ans = 0 , pw = 1;
 while( binary_num > 0){
-int unit_digit = binary_num % 10;
+int unit_digit = binary_num % 10; 
 ans +=(unit_digit * pw);
  binary_num /= 10;
 pw *=2;
