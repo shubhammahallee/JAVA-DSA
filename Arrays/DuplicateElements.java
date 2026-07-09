10be37f4302365eaa65c9f2f72890d05e5850450
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-09 08:43:25 +0530

    Update DuplicateElements.java

--- Arrays/DuplicateElements.java
+++ Arrays/DuplicateElements.java
@@ -1,1 +1,9 @@
-
+public static void duplicateNumber(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate Number: " + arr[i]);
+                }
+            }
+        }
