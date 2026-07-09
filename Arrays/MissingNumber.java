93d268ba0910d34b83bc7aef10d155bba9b44bbb
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-09 08:42:36 +0530

    Update MissingNumber.java

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -1,1 +1,9 @@
-
+    public static void missingNumber(int[] arr) {
+        int n = arr.length;
+        int sum = (n * (n + 1)) / 2;
+        int arrSum = 0;
+        for (int i = 0; i < n; i++) {
+            arrSum += arr[i];
+        }
+        System.out.println("Missing Number: " + (sum - arrSum));
+    }
