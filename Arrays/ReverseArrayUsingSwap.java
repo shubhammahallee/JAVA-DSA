a9a17b1ff5928ff3400b59f2dcb8fda7ac109e50
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-09 08:44:25 +0530

    Update ReverseArrayUsingSwap.java

--- Arrays/ReverseArrayUsingSwap.java
+++ Arrays/ReverseArrayUsingSwap.java
@@ -1,1 +1,14 @@
+ public static void swapArray(int[] arr) {
+        int n = arr.length;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for (int ele : arr)
+            System.out.print(ele + " ");
 
+    }
