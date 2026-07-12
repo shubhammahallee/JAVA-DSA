ffa59df54b6bf1604c8cc90c9e0c0a021f8ee419
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-12 08:38:26 +0530

    Update MirrorPattern.java

--- Recursion/MirrorPattern.java
+++ Recursion/MirrorPattern.java
@@ -1,1 +1,9 @@
-
+public static void printMirrorPattern(int n) {
+    if (n == 1) {
+        System.out.print("1 ");
+        return;
+    }
+    System.out.print(n + " ");
+    printMirrorPattern(n - 1);
+    System.out.print(n + " ");
+}
